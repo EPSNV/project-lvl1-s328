08b52b0f5c35a464250d120d7c07ab611ea0fefe
epsnv <epsnv@yandex.ru>
2018-08-21 12:16:02 +0500

    checkpoint 5. add calc & fix other

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,9 +1,4 @@
 #!/usr/bin/env node
-import { hello, isEven } from '..';
+import { even } from '..';
 
-
-const numOfRightAnswers = 3;
-console.log('Welcome to the Brain Games!');
-console.log('Answer "yes" if number even otherwise answer "no".');
-hello();
-console.log(`${isEven(numOfRightAnswers)}`);
+even();
--- src/index.js
+++ src/index.js
@@ -1,13 +1,46 @@
 import readlineSync from 'readline-sync';
+import { cons, car, cdr } from 'hexlet-pairs';
 
-const name = readlineSync.question('May I have your name? ');
-export const hello = () => console.log(`Hello, ${name}!`);
+export const hello = (description = '') => {
+  console.log('Welcome to the Brain Games!');
+  console.log(description);
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  return name;
+};
 
-export const isEven = (num) => {
-  for (let i = num; i > 0;) {
-    const r = Math.floor(Math.random() * 100);
-    const rightAnswer = r % 2 === 0 ? 'yes' : 'no';
-    console.log(`Questions ${r}`);
+const isEven = number => number % 2 === 0 ? 'yes' : 'no';
+const random = (maxNumber = 100) => Math.floor(Math.random() * maxNumber);
+
+const makeQuestionAnswer = (question, answer) => cons(question, answer);
+const getQuestion = questionAnswer => car(questionAnswer);
+const getAnswer = questionAnswer => cdr(questionAnswer);
+
+const generateEven = () => {
+  const argument = random();
+  return makeQuestionAnswer(argument, isEven(argument));
+};
+
+const generateCalc = () => {
+  const firstArgument = random();
+  const secondArgument = random();
+  const sign = random(3);
+  console.log(sign);
+  switch (sign) { // вот тут мне не нравится, но пока не придумал как упростить.
+    case 0: return makeQuestionAnswer(`${firstArgument}+${secondArgument}`, `${firstArgument + secondArgument}`);
+    case 1: return makeQuestionAnswer(`${firstArgument}-${secondArgument}`, `${firstArgument - secondArgument}`);
+    case 2: return makeQuestionAnswer(`${firstArgument}*${secondArgument}`, `${firstArgument * secondArgument}`);
+    default: return undefined;
+  }
+};
+
+export const Game = (numberRightAnswer, questionAnswerGenerator, description) => {
+  const name = hello(description);
+  for (let i = numberRightAnswer; i > 0;) {
+    const questionAnswer = questionAnswerGenerator();
+    const question = getQuestion(questionAnswer);
+    const rightAnswer = getAnswer(questionAnswer);
+    console.log(`Questions ${question}`);
     const answer = readlineSync.question('Your answer: ');
 
     if (answer === rightAnswer) {
@@ -18,5 +51,36 @@ export const isEven = (num) => {
       console.log(`Let's try again, ${name}!`);
     }
   }
-  return `Congratulations, ${name}!`;
+  console.log(`Congratulations, ${name}!`);
 };
+
+export const even = () => Game(3, generateEven, 'Answer "yes" if number even otherwise answer "no".');
+export const calc = () => Game(3, generateCalc, 'What is the result of the expression?');
+
+/*
+Легкая наркомания :) Пытался через пары создать сущность, которая будет содержать
+два числа, знак и методы для обработки всего этого под цели игры Калькулятор.
+const makeCalcArguments = (firstArgument, secondArgument) => cons(firstArgument, secondArgument);
+const getFirstArgument = calcArguments => car(calcArguments);
+const getSecondArgument = calcArguments => cdr(calcArguments);
+const sumCalcArguments = calcArguments => car(calcArguments) + cdr(calcArguments);
+const subCalcArguments = calcArguments => car(calcArguments) - cdr(calcArguments);
+const multCalcArguments = calcArguments => car(calcArguments) * cdr(calcArguments);
+
+const makeCalc = (sign, calcArguments) => cons(sign, calcArguments);
+const getSign = calc => car(calc);
+const getCalcArguments = calc => cdr(calc);
+const calculate = (calc) => {
+  switch(getSign(calc)) {
+    case '+': return sumCalcArguments(getCalcArguments(calc));
+    case '-': return subCalcArguments(getCalcArguments(calc));
+    case '*': return multCalcArguments(getCalcArguments(calc));
+    default: return undefined;
+  }
+ const calcToString = calc => {
+   firstArgument = getFirstArgument(getCalcArguments(calc));
+   secondArgument = getSecondArgument(getCalcArguments(calc));
+   sign = (getSign(calc));
+   return `${firstArgument}${sign}${secondArgument}`
+};
+*/
