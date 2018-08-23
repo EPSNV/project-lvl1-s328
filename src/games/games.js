774780c9c57ed2933a23a37a2f062c1556c852cd
epsnv <epsnv@yandex.ru>
2018-08-23 23:20:48 +0500

    Colorize :) & make repeat in menu

--- src/games/games.js
+++ src/games/games.js
@@ -1,3 +1,6 @@
+import {
+  red, inverse, green, yellow, bold,
+} from 'colors';
 import readlineSync from 'readline-sync';
 import balance from './balance';
 import even from './even';
@@ -6,19 +9,16 @@ import gcd from './gcd';
 import progression from './progression';
 import prime from './prime';
 
-export default () => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? \n');
-  console.log(`Hello, ${name}!\n`);
-  console.log('Choose game:\n');
-  console.log('1. Even');
-  console.log('2. Calc');
-  console.log('3. GreatCommonDivisor');
-  console.log('4. Balance');
-  console.log('5. Progression');
-  console.log('6. Prime');
-  console.log('Q. Exit \n');
-  const gameChoose = readlineSync.question('Type 1-6 for play or Q for Exit\n');
+const chooseGame = () => {
+  console.log(yellow('Choose game:\n'));
+  console.log('  1. Even');
+  console.log('  2. Calc');
+  console.log('  3. GreatCommonDivisor');
+  console.log('  4. Balance');
+  console.log('  5. Progression');
+  console.log('  6. Prime');
+  console.log('  Q. Exit \n');
+  const gameChoose = readlineSync.question(yellow('Type 1-6 for play or Q for Exit\n'));
   switch (gameChoose) {
     case '1': {
       even();
@@ -45,11 +45,19 @@ export default () => {
       break;
     }
     case 'Q': {
-      console.log(`Goodbye, ${name}! I'll be back ;). \n`);
       return;
     }
     default: {
-      console.log('Incorrect! try again next time \n');
+      console.log(red('\nIncorrect! try again \n'));
     }
   }
+  chooseGame();
+};
+
+export default () => {
+  console.log(inverse('Welcome to the Brain Games!\n'));
+  const name = bold(readlineSync.question('May I have your name? \n'));
+  console.log(green(`\nHello, ${name}!\n`));
+  chooseGame();
+  console.log(red(`\nGoodbye, ${name}! I'll be back ;). \n`));
 };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
+import {
+  red, inverse, green, yellow, bold,
+} from 'colors';
+
 import readlineSync from 'readline-sync';
 import { car, cdr } from 'hexlet-pairs';
 
 const numberRightAnswer = 3;
 
 export default (questionAnswerGenerator, description) => {
-  console.log('Welcome to the Brain Games!');
-  console.log(description);
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
+  console.log(inverse('\nWelcome to the Brain Games!\n'));
+  console.log(yellow(description));
+  const name = bold(readlineSync.question('\nMay I have your name?\n'));
+  console.log(green(`\nHello, ${name}!\n`));
   for (let i = 0; i < numberRightAnswer; i += 1) {
     const questionAnswer = questionAnswerGenerator();
     const question = car(questionAnswer);
@@ -16,12 +20,12 @@ export default (questionAnswerGenerator, description) => {
     const answer = readlineSync.question('Your answer: ');
 
     if (answer === rightAnswer) {
-      console.log('Correct!');
+      console.log(green('Correct!\n'));
     } else {
-      console.log(`${answer} is wrong answer ;(. Correct answer was ${rightAnswer}.`);
-      console.log(`Let's try again, ${name}! \n`);
+      console.log(red(`${answer} is wrong answer ;(. Correct answer was ${rightAnswer}.`));
+      console.log(red(`Let's try again, ${name}! \n`));
       return;
     }
   }
-  console.log(`Congratulations, ${name}! \n`);
+  console.log(green(`Congratulations, ${name}! \n`));
 };
